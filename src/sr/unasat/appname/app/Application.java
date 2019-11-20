6ba7962d5b34771c46fe9951d61794d73ec55c17
Zareef <zareefsoeto@gmail.com>
2019-11-19 22:43:45 -0300

    Initial commit

---
+++ src/sr/unasat/appname/app/Application.java
@@ -0,0 +1,17 @@
+/*
+In this package you can find the main class.
+All the directories needs to be inserted here.
+All the libraries needed needs to be imported here.
+Author: Zareef Soetodrono.
+Date:   19 november 2019.
+Email:  zareef.soetodrono@gmail.com
+ */
+
+package sr.unasat.appname.app;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
